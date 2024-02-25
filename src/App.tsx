1d2bc833f1377262d33afa87922fce08de4a0c18
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-25 13:37:49 -0300

    Aula 3.1. Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { getEventsEndpoint } from './backend';
+// import { getEventsEndpoint } from './backend';
+import Button from '@mui/material/Button';
 
 function App() {
-  getEventsEndpoint().then(events => {
-    for (const event of events) {
-      console.log(event);
-    }
-  });
-  return <div>Olá</div>;
+  // getEventsEndpoint().then(events => {
+  //   for (const event of events) {
+  //     console.log(event);
+  //   }
+  // });
+  return <Button variant="contained">Hello world</Button>;
 }
 
 export default App;
